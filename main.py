2981d0ab9d5c1a1f43defb05752d853ff7d8f4c8
= <medoxz543@gmail.com>
2024-03-14 20:51:44 +0200

    ..

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from dash import Dash
 from src.layout import create_layout
 from dash_bootstrap_components.themes import BOOTSTRAP
 
-
-app = Dash(external_stylesheets=[BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
 server = app.server
 app.title = "Walmart Project"
 app.layout = create_layout(app)
-app.run()
+
+if __name__ == "__main__":
+    app.run_server()
--- src/graph_elements/first_tab_graph.py
+++ src/graph_elements/first_tab_graph.py
@@ -9,8 +9,8 @@ def render(app: Dash) -> html.Div:
     # Add controls to build the interaction
     @app.callback(
         Output(component_id=ids.BAR_CHART, component_property="figure"),
-        Output(component_id=ids.SELECTOR_DROPDOWN, component_property='style'),
-        Output(component_id=ids.FIRST_TAB_H5, component_property='style'),
+        # Output(component_id=ids.SELECTOR_DROPDOWN, component_property='style'),
+        # Output(component_id=ids.FIRST_TAB_H5, component_property='style'),
         Input(component_id=ids.IDENTIFIER_RADIO_ITEMS, component_property='value'),
         Input(component_id=ids.SELECTOR_DROPDOWN, component_property='value')
     )
@@ -19,14 +19,15 @@ def render(app: Dash) -> html.Div:
         # Your existing code for chart update
         figure = select_and_prepare_dashboard_barcharts(load_data(), identifier, select_type)
         
-        # Modify this line to update dropdown visibility based on the condition
-        dropdown_style = {'width': '180px', 'background-color': '#343a40', 'color': '#1E88E5'}
-        h5_style = {"color": '#FFAA00'}
+        # # Modify this line to update dropdown visibility based on the condition
+        # dropdown_style = {'width': '180px', 'background-color': '#343a40', 'color': '#1E88E5'}
+        # h5_style = {"color": '#FFAA00'}
 
-        if identifier in ["Dept Sales: Holiday vs Non-Holiday", "Top 5 Departments", "Bottom 5 Departments"]:
-            dropdown_style['display'] = 'none' 
-            h5_style['display'] = 'none' 
+        # if identifier in ["Dept Sales: Holiday vs Non-Holiday", "Top 5 Departments", "Bottom 5 Departments"]:
+        #     dropdown_style['display'] = 'none' 
+        #     h5_style['display'] = 'none' 
 
-        return figure, dropdown_style, h5_style
+        return figure
+    # , dropdown_style, h5_style
     
     return dcc.Graph(id=ids.BAR_CHART)
